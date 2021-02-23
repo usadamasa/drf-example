5335b1b082f37dcf82b1491b9c6cf4abb8b949d2
usadamasa <usadamasa@gmail.com>
2021-02-23 22:50:49 +0900

    complate snippets

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from typing import List
 
-# Create your views here.
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+
+from tutorial.snippets.models import Snippet
+from tutorial.snippets.serializers import SnippetSerializer
+
+
+@csrf_exempt
+def snippet_list(request) -> HttpResponse:
+    """
+
+    """
+    if request.method == 'GET':
+        snippets: List[Snippet] = Snippet.objects.all()
+        serializer: SnippetSerializer = SnippetSerializer(instance=snippets, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer: SnippetSerializer = SnippetSerializer(data=data)
+        if not serializer.is_valid():
+            return JsonResponse(serializer.errors, status=400)
+        serializer.save()
+        return JsonResponse(serializer.data, status=201)
+
+
+@csrf_exempt
+def snippet_detail(request, pk) -> HttpResponse:
+    """
+    Retrieve, update or delete a code snippet.
+    """
+    try:
+        snippet = Snippet.objects.get(pk=pk)
+    except Snippet.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = SnippetSerializer(snippet)
+        return JsonResponse(serializer.data)
+
+    if request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(snippet, data=data)
+        if not serializer.is_valid():
+            return JsonResponse(serializer.errors, status=400)
+        serializer.save()
+        return JsonResponse(serializer.data)
+
+    if request.method == 'DELETE':
+        snippet.delete()
+        return HttpResponse(status=204)
--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,14 +1,16 @@
 from django.urls import include, path
 from rest_framework import routers
-from tutorial.quickstart import views
+
+from tutorial.quickstart import views as quickstart_views
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+router.register(r'users', quickstart_views.UserViewSet)
+router.register(r'groups', quickstart_views.GroupViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include('tutorial.snippets.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
