a557e449e00c92a7c3af5f9086b641c979dd8ab3
usadamasa <usadamasa@gmail.com>
2021-02-23 22:33:39 +0900

    Using ModelSerializers

---
+++ tutorial/snippets/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+from tutorial.snippets.models import Snippet
+
+
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = [
+            'id',
+            'title',
+            'code',
+            'linenos',
+            'language',
+            'style',
+        ]
